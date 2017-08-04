3b392403d63e8f591feb27229fd3bf3e901367f7
Rafael Said <rafaelsaidbc@yahoo.com.br>
2017-08-04 15:49:58 -0300

    Change Price to Order Summary in all code

--- AndroidStudioProjects/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ AndroidStudioProjects/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -65,20 +64,12 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
     /*This method displays the given text on the screen.
     */
 
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
 
